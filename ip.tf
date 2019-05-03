3be207f6e82f8148734ffd014e47f82563d93e6d
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-03 15:53:20 +1200

    Add a comment

--- ip.tf
+++ ip.tf
@@ -1,4 +1,17 @@
 // A private IP address for database peering
+/* Please note:
+   In some cases, for example when an IP address was recreated,
+   GCP API reports that it can't remap the address (range).
+
+   To fix this error, execute the command below from the console:
+
+gcloud beta services vpc-peerings update \
+    --service=servicenetworking.googleapis.com \
+    --ranges=<PUT YOUR IP ADDRESS NAME HERE> \
+    --network=common-gke-us-us-central1-network \
+    --project=unimarket-terraform-dev \
+    --force
+*/
 resource "google_compute_global_address" "this" {
   provider      = "google-beta"
   name          = "${local.ip_address_name}"
