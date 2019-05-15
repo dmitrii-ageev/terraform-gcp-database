a3dbb3ae58edb4d708f03bdc2f70fa8b648db19d
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-16 09:38:06 +1200

    Add an index to a database instance resource

--- locals.tf
+++ locals.tf
@@ -3,10 +3,10 @@ locals {
   create_instance = "${var.instance == "" ? 1 : 0}"
 
   // The database instance to use
-  database_instance = "${var.instance == "" ? google_sql_database_instance.this.self_link : var.instance }"
+  database_instance = "${var.instance == "" ? google_sql_database_instance.this.0.self_link : var.instance }"
 
   // The database instance name
-  database_instance_name = "${var.instance_name == "" ? google_sql_database_instance.this.name : var.instance_name }"
+  database_instance_name = "${var.instance_name == "" ? google_sql_database_instance.this.0.name : var.instance_name }"
 
   // The private IP address name
   ip_address_name = "${var.instance_name == "" ? "database" : var.instance_name}-private-ip-address"
